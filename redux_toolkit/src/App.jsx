ba24ac88cc0ec89d17930e4ef45f0a3076bd3374
Pavan Shinde <pavanshinde3304@gmail.com>
2023-12-20 11:10:33 +0530

    second commit

---
+++ redux_toolkit/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+      <Todos />
+      <AddTodo />
+
+    </>
+  )
+}
+
+export default App
+
+
+
